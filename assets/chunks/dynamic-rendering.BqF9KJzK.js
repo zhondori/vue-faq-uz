c4be4c180ed63f009624c39ec522866e3bbd2e4b
zhondori <83527629+zhondori@users.noreply.github.com>
2024-05-06 03:32:28 +0000

    Deploying to gh-pages from @ zhondori/vue-faq-uz@17bbcf46bb9d5217730faac0df9b27980b33db26 🚀

--- assets/chunks/dynamic-rendering.BqF9KJzK.js
+++
@@ -1,1 +0,0 @@
-const s="/uz/assets/spa-vs-mpa.nrTvFI86.png",n="/uz/assets/dynamic-rendering.khOwGjEn.png";export{s as _,n as a};
--- assets/chunks/modular-architecture.CmNqooEU.js
+++
@@ -1,1 +0,0 @@
-const s="/uz/assets/modular-architecture.DS-xp6qR.webp";export{s as _};
